d7e7ca859ea8a668020f94303c346a5b7855ddab
Lucas Melchior <lucas.melchior@dpu.def.br>
2023-12-21 04:44:51 -0300

    Criando o component Header da aplicação e estilizando com Tailwild

---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import logo from "../assets/github-logo.png";
+import logoName from "../assets/github-logo-name.png";
+import { Link } from "react-router-dom";
+
+export function Header() {
+  return (
+    <div className="flex bg-gray-800  absolute w-full max-auto items-center p-4 top-0 gap-2 h-20">
+      <img src={logo} alt="logo" width={50} height={50} />
+      <img
+        src={logoName}
+        alt="logo"
+        width={60}
+        height={60}
+        className="mr-[20rem]"
+      />
+
+      <div className="flex items-center justify-center gap-8">
+        <Link
+          to="/"
+          className="text-white w-full text-md font-normal hover:text-gray-200"
+        >
+          Home
+        </Link>
+        <Link
+          to="/user"
+          className="text-white w-full text-md font-normal hover:text-gray-200"
+        >
+          Buscar
+        </Link>
+      </div>
+    </div>
+  );
+}
