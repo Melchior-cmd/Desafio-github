1459361dc2a64fc92d6bd079499d591a26f439d1
Lucas Melchior <lucas.melchior@dpu.def.br>
2023-12-24 16:23:21 -0300

    Implementando a funcionalidade de  Notificação de erros ao pesquisar usuário no github.

--- src/pages/SearchUser.tsx
+++ src/pages/SearchUser.tsx
@@ -5,14 +5,44 @@ import { Search } from "../components/Search";
 import { api } from "../services/api";
 import { Header } from "../components/Header";
 import { UserCard } from "../components/UserCard";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import { Spinner } from "../components/Spinner";
 import { BackButton } from "../components/BackButton";
 
 export function SearchUser() {
   const [user, setUser] = useState<UserProps | null>(null);
-
   const [isLoading, setIsLoading] = useState(false);
+
+  const UserSuccess = () => {
+    toast.success("Usuário localizado com sucesso!.", {
+      position: "top-right",
+      autoClose: 4000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      containerId: "A",
+    });
+  };
+
+  const UserErrorNotification = () => {
+    toast.error(
+      "Usuário não encontrado!, por favor preencha um usuário valido.",
+      {
+        position: "top-right",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        containerId: "B",
+      }
+    );
+  };
 
   const loadUser = async (username: string) => {
     try {
@@ -37,7 +67,9 @@ export function SearchUser() {
       };
 
       setUser(userData);
+      UserSuccess();
     } catch (error) {
+      UserErrorNotification();
       console.error("Erro na requisição:", error);
     } finally {
       setIsLoading(false);
@@ -49,6 +81,8 @@ export function SearchUser() {
       <Header />
       <BackButton />
       <div className=" w-full h-screen bg-gray-700 ">
+        <ToastContainer enableMultiContainer containerId={"A"} />
+        <ToastContainer enableMultiContainer containerId={"B"} />
         <div className="flex flex-col  h-screen  justify-center items-center ">
           <Search loadUser={loadUser} />
 
